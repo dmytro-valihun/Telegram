5eca69a77bd2613ec6e0590ea2a50276d434341d
Dmytro Valihun <dmytro.valihun@gmail.com>
2023-01-14 17:17:29 +0100

    created js for app

--- js/app.js
+++ js/app.js
@@ -1,10 +1,54 @@
-// const login = document.querySelector('.first_screen');
-// login.hidden = true;
-// const mainScreenLeft = document.querySelector('.left_side');
-// mainScreenLeft.hidden = true;
-// const mainScreenRight = document.querySelector('.right_side');
-// mainScreenRight.hidden = true;
-// const exitWindow = document.querySelector('.exit_pop');
-// exitWindow.hidden = true;
-// const logOut = document.querySelector('.user_info_pop');
-// logOut.hidden = false
+const loginWindow = document.querySelector('.login');
+const loginForm = document.querySelector('.login__input-login');
+const passForm = document.querySelector('.login__input-pass');
+const btnGoLogin = document.querySelector('.login__btn');
+const messageWindow = document.querySelector('.message-window');
+messageWindow.style.display = 'none';
+const btnUserMenu = document.querySelector('.burger-menu__wrap');
+const userInfoMenu = document.querySelector('.user-info');
+const btnCloseUserInfo = document.querySelector('.user-info__close-btn');
+const exitPopupWindow = document.querySelector('.exit-popup-wrap');
+exitPopupWindow.hidden = true;
+const btnExit = document.querySelector('.user-info__btn');
+const btnExitYes = document.querySelector('.exit-btn__yes');
+const btnExitNo = document.querySelector('.exit-btn__no');
+
+btnGoLogin.addEventListener('click', enterInAccount);
+btnUserMenu.addEventListener('click', showUserInfo);
+btnCloseUserInfo.addEventListener('click', closeUserInfo);
+btnExit.addEventListener('click', showPopupExit);
+btnExitYes.addEventListener('click', showLoginPage);
+btnExitNo.addEventListener('click', closePopupWindow);
+
+function enterInAccount() {
+    if (localStorage.length === 0) {
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+    const userList = JSON.parse(localStorage.getItem('users'));
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].phone === loginForm.value && users[i].password === passForm.value) {
+            loginWindow.style.display = 'none';
+            messageWindow.style.display = 'flex';
+        }
+    }
+}
+function showUserInfo() {
+    userInfoMenu.style.left = 0;
+}
+function closeUserInfo() {
+    userInfoMenu.style.left = '-100%';
+}
+function showPopupExit() {
+    exitPopupWindow.hidden = false;
+}
+function showLoginPage() {
+    loginWindow.style.display = 'flex';
+    messageWindow.style.display = 'none';
+    userInfoMenu.style.left = '-100%';
+    exitPopupWindow.hidden = true;
+    loginForm.value = '';
+    passForm.value = '';
+}
+function closePopupWindow() {
+    exitPopupWindow.hidden = true;
+}
---
+++ js/data.js
@@ -0,0 +1,6 @@
+const users = [
+    {name: 'Vania Kupkin', password: '1', phone: '1'},
+    {name: 'Vasia Pupkin', password: '123456Qw', phone: '+380991231212'},
+    {name: 'Masha Dupkina', password: '123456As', phone: '+380501231212'},
+    {name: 'Petia Lupkin', password: '123456Zx', phone: '+380661231212'},
+]
