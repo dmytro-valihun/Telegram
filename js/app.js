05ddc22f3aa1257eaa5dd4b6b3013e0c903ccc9c
Dmytro Valihun <dmytro.valihun@gmail.com>
2023-01-16 01:50:12 +0100

    added close btn

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const messageWindow = document.querySelector('.message-window');
 messageWindow.style.display = 'none';
 const btnUserMenu = document.querySelector('.burger-menu__wrap');
 const userInfoMenu = document.querySelector('.user-info');
-const btnCloseUserInfo = document.querySelector('.user-info__close-btn');
+const btnCloseUserInfo = document.querySelector('.close-btn-user-info');
 const exitPopupWindow = document.querySelector('.exit-popup-wrap');
 exitPopupWindow.hidden = true;
 const btnExit = document.querySelector('.user-info__btn');
@@ -26,6 +26,8 @@ const windowPassChangeOk = document.querySelector('.popup-pass-changed');
 windowPassChangeOk.style.display = 'none';
 const btnPassChangeOk = document.querySelector('.popup-pass-changed__ok');
 btnPassChangeOk.addEventListener('click', closeWindowChangePass);
+const btnCancelSetNewPass = document.querySelector('.close-btn-cancel-new-pass');
+btnCancelSetNewPass.addEventListener('click', cancelSetNewPass);
 
 
 btnGoLogin.addEventListener('click', enterInAccount);
@@ -40,8 +42,8 @@ function enterInAccount() {
         localStorage.setItem('users', JSON.stringify(users));
     }
     const userList = JSON.parse(localStorage.getItem('users'));
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].phone === loginForm.value && users[i].password === passForm.value) {
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].phone === loginForm.value && userList[i].password === passForm.value) {
             loginWindow.style.display = 'none';
             messageWindow.style.display = 'flex';
         } else {
@@ -85,14 +87,17 @@ function changeUserPass() {
     const repeatUserNewPass = document.forms['createNewPass'].repeatUserNewPass.value;
     const pass_1 = checkPass(userNewPass);
     const pass_2 = checkPass(repeatUserNewPass);
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].name === userName && pass_1 === true && pass_1 === pass_2) {
-            const userList = JSON.parse(localStorage.getItem('users'));
+    const userList = JSON.parse(localStorage.getItem('users'));
+
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].name === userName && pass_1 === true && userNewPass === repeatUserNewPass) {
+            // const userList = JSON.parse(localStorage.getItem('users'));
+            console.log(userList)
+            console.log(pass_1)
+            console.log(pass_2)
             userList[i].password = userNewPass;
             localStorage.setItem('users', JSON.stringify(userList));
-            console.log(userList);
             divCreateNewUserPass.style.display = 'none';
-            // loginWindow.style.display = 'flex';
             divWrongChangePass.style.display = 'none';
             windowPassChangeOk.style.display = 'block';
         } else {
@@ -110,5 +115,13 @@ function checkPass(string) {
 }
 function closeWindowChangePass() {
     windowPassChangeOk.style.display = 'none';
+    loginWindow.style.display = 'flex';
+}
+
+function cancelSetNewPass() {
+    divCreateNewUserPass.style.display = 'none';
+    divWrongPass.style.display = 'none';
+    loginForm.value = '';
+    passForm.value = '';
     loginWindow.style.display = 'flex';
 }
