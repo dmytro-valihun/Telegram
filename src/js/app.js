dee67a6a6d9da5047a858c2ed4f867c9ce863440
Dmytro Valihun <dmytro.valihun@gmail.com>
2023-03-05 23:42:54 +0100

    add loader for user from json file

--- src/js/app.js
+++ src/js/app.js
@@ -1,54 +1,51 @@
-const users = [
-    {name: 'Vania Kupkin', password: '1', phone: '1'},
-    {name: 'Valia Rupkina', password: '2', phone: '2'},
-    {name: 'Vasia Pupkin', password: '123456Qw', phone: '+380991231212'},
-    {name: 'Masha Dupkina', password: '123456As', phone: '+380501231212'},
-    {name: 'Petia Lupkin', password: '123456Zx', phone: '+380661231212'},
-] 
-// /const messages = [];
-let messages = JSON.parse(localStorage.getItem('messages')) || [];
+const messages = [];
+// let existMessages = JSON.parse(localStorage.getItem('messages')) || [];
 
 const loginWindow = document.querySelector('.login');
 const loginForm = document.querySelector('.login__input-login');
 const passForm = document.querySelector('.login__input-pass');
 const btnGoLogin = document.querySelector('.login__btn');
 const messageWindow = document.querySelector('.message-window');
-messageWindow.style.display = 'none';
 const btnUserMenu = document.querySelector('.burger-menu__wrap');
 const userInfoMenu = document.querySelector('.user-info');
 const btnCloseUserInfo = document.querySelector('.close-btn-user-info');
 const exitPopupWindow = document.querySelector('.exit-popup-wrap');
-exitPopupWindow.hidden = true;
 const btnExit = document.querySelector('.user-info__btn');
 const btnExitYes = document.querySelector('.exit-btn__yes');
 const btnExitNo = document.querySelector('.exit-btn__no');
 const divWrongPass = document.querySelector('.login__wrong-message');
-divWrongPass.style.display = 'none';
 const btnForgotPassword = document.querySelector('.login__btn-forgote-message');
-btnForgotPassword.addEventListener('click', createNewPassword);
 const divCreateNewUserPass = document.querySelector('.create-new-password');
-divCreateNewUserPass.style.display = 'none';
 const divWrongChangePass = document.querySelector('.create-new-password__wrong-message');
-divWrongChangePass.style.display = 'none';
 const btnChangeUserPass = document.querySelector('.create-new-password__btn');
-btnChangeUserPass.addEventListener('click', changeUserPass);
 const windowPassChangeOk = document.querySelector('.popup-pass-changed');
-windowPassChangeOk.style.display = 'none';
 const btnPassChangeOk = document.querySelector('.popup-pass-changed__ok');
-btnPassChangeOk.addEventListener('click', closeWindowChangePass);
 const btnCancelSetNewPass = document.querySelector('.close-btn-cancel-new-pass');
-btnCancelSetNewPass.addEventListener('click', cancelSetNewPass);
-const userList = JSON.parse(localStorage.getItem('users'));
 const messChating = document.querySelector('.message__chatting');
-messChating.style.display = 'none';
 const btnSend = document.querySelector('.btn-send');
 const btnSmiles = document.querySelector('.btn-smiles');
 const textMessage = document.querySelector('.message__text');
 const messagesWindow = document.querySelector('.message__messeges');
 const userName = document.querySelector('.user-info__name');
 const smilesList = document.querySelector('.message__smiles');
-smilesList.style.display = 'none';
+let userList; // сюда запишем подтянутых юзеров
+let currentInterlocutor; // чел с которым общается шлавный юзер
+let messageHeight = 100;// данные для увеличения окна набора смс
 
+exitPopupWindow.hidden = true;
+divWrongChangePass.style.display = 'none';
+divCreateNewUserPass.style.display = 'none';
+divWrongPass.style.display = 'none';
+smilesList.style.display = 'none';
+messChating.style.display = 'none';
+windowPassChangeOk.style.display = 'none';
+messageWindow.style.display = 'none';
+
+btnChangeUserPass.addEventListener('click', changeUserPass);
+btnPassChangeOk.addEventListener('click', closeWindowChangePass);
+btnCancelSetNewPass.addEventListener('click', cancelSetNewPass);
+btnChangeUserPass.addEventListener('click', changeUserPass);
+btnForgotPassword.addEventListener('click', createNewPassword);
 btnSend.addEventListener('click', sendMessage);
 btnGoLogin.addEventListener('click', enterInAccount);
 btnUserMenu.addEventListener('click', showUserInfo);
@@ -59,33 +56,44 @@ btnExitNo.addEventListener('click', closePopupWindow);
 btnSmiles.addEventListener('click', btnSmilesHandler);
 
 
-function enterInAccount() {
-    if (localStorage.length === 0) {
-        localStorage.setItem('users', JSON.stringify(users));
-    }
+// логинимся
+function enterInAccount () {
+    const usersArr = [];
+    fetch('http://127.0.0.1:5501/build/users.json')
+    .then(res => res.json())
+    .then(result => {
+        usersArr.push(...result);
+        finalEnter(usersArr)
+    })
+}
+// логинимся
+function finalEnter(arr) {
+    userList = arr;
     messagesWindow.textContent = '';
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i].phone === loginForm.value && userList[i].password === passForm.value) {
-            loginWindow.style.display = 'none';
-            messageWindow.style.display = 'flex';
-            const userPhone = document.querySelector('.user-info__tel');
-            userName.textContent = userList[i].name;
-            userPhone.textContent = userList[i].phone;
-            createList(i);
-        } else {
-            divWrongPass.style.display = 'block';
-        }
+    const matchesUser = userList.find(user => user.phone === +loginForm.value && user.password === +passForm.value);
+    if (matchesUser) {
+        loginWindow.style.display = 'none';
+        messageWindow.style.display = 'flex';
+        const userPhone = document.querySelector('.user-info__tel');
+        userName.textContent = matchesUser.name;
+        userPhone.textContent = matchesUser.phone;
+        createList(matchesUser.id);
+    } else {
+        divWrongPass.style.display = 'block';
     }
 }
-//  Инфо о пользователе. Левая панель
+
+//  Инфо о пользователе. Левая панель показать
 function showUserInfo() {
     userInfoMenu.style.left = 0;
 }
+//  Инфо о пользователе. Левая панель скрыть
 function closeUserInfo() {
     userInfoMenu.style.left = '-100%';
 }
+
 //  Создание списка контактов на левой панели
-function createList (number) {
+function createList(number) {
     messChating.style.display = 'none';
     const chatList = document.querySelector('.chat-list__user-list');
     chatList.textContent = "";
@@ -101,7 +109,7 @@ function createList (number) {
     }
 }
 
-let currentInterlocutor;
+
 
 //Начало общения
 function chattingStart(event){
@@ -121,7 +129,7 @@ function chattingStart(event){
 
     const history = [];
     console.log(222, messages);
-    for (let i = 0; i<messages.length; i++){
+    for (let i = 0; i < messages.length; i++){
         // console.log (messages[i].currentUser === currentUser)
        if ((messages[i].currentUser === currentUser)&&(messages[i].currentInterlocutor === currentInterlocutor)){
         // console.log(messages[i].time, messages[i].sms);
@@ -134,20 +142,7 @@ function chattingStart(event){
         createElements('div', `${messages[i].sms}`, 'message__from-friend', messagesWindow.lastChild);
        }
     }
-
-
 }
-
-//увеличиваем текстовое окно
-let messageHeight = 100;
-
-textMessage.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13) {
-        if (messageHeight > 270) return;
-        textMessage.style.height = `${messageHeight}px`;
-        messageHeight += 20;
-    }
-})
 
 
 
@@ -240,11 +235,10 @@ function btnSmilesHandler(){
         smilesList.style.display = 'none'; 
     }
     else {
-        smilesList.style.display = 'block'
+        smilesList.style.display = 'block';
     }       
 }
 addUnicode=function(n){
-    
     let text=document.querySelector('.message__text');
     let s=String.fromCodePoint(n);
     console.log(s);
@@ -258,6 +252,10 @@ addUnicode=function(n){
 	else text.value+=s;
 }
 
+
+
+
+// хелпер - создать елемент
 function createElements(elem, text, className, parrent) {
     const element = document.createElement(elem);
     element.textContent = text;
@@ -265,6 +263,14 @@ function createElements(elem, text, className, parrent) {
     parrent.appendChild(element);
 }
 
+//увеличиваем текстовое окно
+textMessage.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13) {
+        if (messageHeight > 270) return;
+        textMessage.style.height = `${messageHeight}px`;
+        messageHeight += 20;
+    }
+})
 
 // Выход из аккаунта
 function showPopupExit() {
@@ -281,9 +287,13 @@ function showLoginPage() {
     loginForm.value = '';
     passForm.value = '';
 }
+
+// кнопка не выходить с аккаунта
 function closePopupWindow() {
     exitPopupWindow.hidden = true;
 }
+
+// кнопка если забыл пароль
 function createNewPassword() {
     document.forms['createNewPass'].userName.value = '';
     document.forms['createNewPass'].userNewPass.value = '';
@@ -292,6 +302,8 @@ function createNewPassword() {
     loginWindow.style.display = 'none';
     divCreateNewUserPass.style.display = 'flex';
 }
+
+// меняем пароль
 function changeUserPass() {
     divWrongPass.style.display = 'none';
     const userName = document.forms['createNewPass'].userName.value;
@@ -299,24 +311,37 @@ function changeUserPass() {
     const repeatUserNewPass = document.forms['createNewPass'].repeatUserNewPass.value;
     const pass_1 = checkPass(userNewPass);
     const pass_2 = checkPass(repeatUserNewPass);
-    const userList = JSON.parse(localStorage.getItem('users'));
 
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i].name === userName && pass_1 === true && userNewPass === repeatUserNewPass) {
-            // const userList = JSON.parse(localStorage.getItem('users'));
-            console.log(userList)
-            console.log(pass_1)
-            console.log(pass_2)
-            userList[i].password = userNewPass;
-            localStorage.setItem('users', JSON.stringify(userList));
-            divCreateNewUserPass.style.display = 'none';
-            divWrongChangePass.style.display = 'none';
-            windowPassChangeOk.style.display = 'block';
-        } else {
-            divWrongChangePass.style.display = 'flex';
-        }
+    if ((userNewPass === repeatUserNewPass) && pass_1 === true) {
+        checkUsers(userName, userNewPass);
+    } else {
+        divWrongChangePass.style.display = 'flex';
     }
 }
+// меняем пароль
+const checkUsers = (userName, userNewPass) => {
+    const usersArr = [];
+    fetch('http://127.0.0.1:5501/build/users.json')
+    .then(res => res.json())
+    .then(result => {
+        usersArr.push(...result);
+        finalChange(usersArr, userName, userNewPass)
+    })
+}
+// меняем пароль
+function finalChange(arr, userName, userNewPass) {
+    const matchesPass = arr.find(user => user.name === userName);
+    if (matchesPass) {
+        divCreateNewUserPass.style.display = 'none';
+        divWrongChangePass.style.display = 'none';
+        windowPassChangeOk.style.display = 'block';
+        //надо тут записать новый пароль в json чтобы он был доступен, а так остается старый пароль
+        // matchesPass.password = userNewPass => и это сплайсом вставить в бекенд
+    } else {
+        divWrongChangePass.style.display = 'flex';
+    }
+}
+// проверка нового пароля
 function checkPass(string) {
     //ЭТИ 2 СТРОКИ НИЖЕ РАСКОММЕНТИРОВАТЬ, А 2 НИЖНИЕ УДАЛИТЬ!!!
     // const pattern = /^(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])[0-9a-zA-Z]{8,16}$/;
@@ -325,11 +350,12 @@ function checkPass(string) {
     const result = pattern.test(string);
     return result ? true : false;
 }
+// закрытие окна смены пароля
 function closeWindowChangePass() {
     windowPassChangeOk.style.display = 'none';
     loginWindow.style.display = 'flex';
 }
-
+// принудительное закрытие окна смены пароля
 function cancelSetNewPass() {
     divCreateNewUserPass.style.display = 'none';
     divWrongPass.style.display = 'none';
