80b20478e2dc83c5eb09cbfb35637d9d1c61f87a
Dmytro Valihun <dmytro.valihun@gmail.com>
2023-03-08 22:36:26 +0100

    final version

--- src/js/validators.js
+++ src/js/validators.js
@@ -1,9 +1,6 @@
 // проверка нового пароля
 function checkPass(string) {
-    //ЭТИ 2 СТРОКИ НИЖЕ РАСКОММЕНТИРОВАТЬ, А 2 НИЖНИЕ УДАЛИТЬ!!!
-    // const pattern = /^(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])[0-9a-zA-Z]{8,16}$/;
-    // const result = pattern.test(string);
-    const pattern = /\d/;
+    const pattern = /^(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])[0-9a-zA-Z]{8,16}$/;
     const result = pattern.test(string);
     return result ? true : false;
 }
